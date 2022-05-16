9288243ac73295c305df82b369d91d63c3d31b4a
Enzo <enzowederma@gmail.com>
2022-05-16 16:19:11 -0300

    Uploading repo to github.

---
+++ html/js/script.js
@@ -0,0 +1,50 @@
+// Side-Menu Height
+
+function setHeight() {
+    let main = document.querySelector('body');
+    let mainheight = main.offsetHeight;
+    document.querySelector('.menu-items').style.height = mainheight + "px";
+}
+
+setHeight()
+
+window.addEventListener('resize', function(event) {
+    setHeight()
+    }, true);
+
+
+// Dark Mode
+
+var checkbox = document.querySelector(".dark-checkbox");
+
+function DarkMode() {
+    if (checkbox.checked == true) {
+        var link = document.querySelector('link[href$="dark_mode.css"]')
+        link.removeAttribute('disabled')
+
+    } else {
+        var link = document.querySelector('link[href$="dark_mode.css"]')
+        link.setAttribute('disabled', 'true')}   
+};
+
+
+// localStorage
+
+var i, checkboxes = document.querySelectorAll(".dark-checkbox");
+
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked); 
+    }
+}
+
+function load_() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
+    }
+    DarkMode()
+} 
+
+// firstLoad
+
+load_()
