0bab9b1b176a9da8917985c9780966b0afa7b708
Enzo <enzowederma@gmail.com>
2022-05-31 17:52:38 -0300

    Working play video on mouse over Prototype

--- js/videos.js
+++ js/videos.js
@@ -8,3 +8,18 @@ function pause(i) {
 function play(i) { 
     mainVideo[i].play()
 }
+
+mainImg[0].onmouseover = function() { play(0) };
+mainImg[0].onmouseout = function() { pause(0) };
+
+mainImg[1].onmouseover = function() { play(1) };
+mainImg[1].onmouseout = function() { pause(1) };
+
+mainImg[2].onmouseover = function() { play(2) };
+mainImg[2].onmouseout = function() { pause(2) };
+
+mainImg[3].onmouseover = function() { play(3) };
+mainImg[3].onmouseout = function() { pause(3) };
+
+mainImg[4].onmouseover = function() { play(4) };
+mainImg[4].onmouseout = function() { pause(4) };
